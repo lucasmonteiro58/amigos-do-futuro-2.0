e112405b744548408caafa6acb50fcafeebc8b00
LUCAS MONTEIRO RIBEIRO <lucas.monteiro58@gmail.com>
2023-06-19 22:04:45 -0300

    perf: 🚀sprite generator

--- scripts/spritesheet-generator.js
+++ scripts/spritesheet-generator.js
@@ -1,89 +1,59 @@
 /* eslint-disable no-undef */
 const fs = require('fs')
+const util = require('util')
 const path = require('path')
 const Spritesmith = require('spritesmith')
 const glob = require('glob')
 
-// const inputDir = 'src/assets/images/sprites'
-// const outputDir = 'src/assets/images/'
 const defaultClass = 'spritesheet'
-const outputDir = 'src/assets/images/'
+const outputDir = 'src/assets/images/generated-sprites'
+const outputCssFile = 'src/assets/style/_spritesheet.css'
 
 const sprites = [
-  {
-    id: 0,
-    name: 'bg'
-  },
-  {
-    id: 1,
-    name: 'btn-badge-box'
-  },
-  {
-    id: 2,
-    name: 'mapa'
-  },
-  {
-    id: 3,
-    name: 'quiz'
-  },
-  {
-    id: 4,
-    name: 'economia'
-  },
-  {
-    id: 5,
-    name: 'educacao'
-  },
-  {
-    id: 6,
-    name: 'governo'
-  },
-  {
-    id: 7,
-    name: 'inovacao'
-  },
-  {
-    id: 8,
-    name: 'lazer'
-  },
-  {
-    id: 9,
-    name: 'meio'
-  },
-  {
-    id: 10,
-    name: 'saude'
-  }
+  { id: 0, name: 'bg' },
+  { id: 1, name: 'btn-badge-box' },
+  { id: 2, name: 'mapa' },
+  { id: 3, name: 'quiz' },
+  { id: 4, name: 'economia' },
+  { id: 5, name: 'educacao' },
+  { id: 6, name: 'governo' },
+  { id: 7, name: 'inovacao' },
+  { id: 8, name: 'lazer' },
+  { id: 9, name: 'meio' },
+  { id: 10, name: 'saude' }
 ]
 
-sprites.forEach((sprite) => {
-  deleteFiles(generateDeleteArray(), sprite.id)
-  const inputDir = `src/assets/images/sprites/${sprite.name}`
-  generateSprite(inputDir, outputDir, sprite.id)
-})
+const unlink = util.promisify(fs.unlink)
+const writeFile = util.promisify(fs.writeFile)
 
-function generateDeleteArray() {
-  const deleteArray = []
-  sprites.forEach((sprite) => {
-    deleteArray.push(`src/assets/images/spritesheet${sprite.id}.png`)
-    deleteArray.push(`src/assets/style/spritesheet${sprite.id}.css`)
-  })
-  return deleteArray
+async function generateSprites() {
+  console.log('🚀 Generating sprites...')
+  for (const sprite of sprites) {
+    console.log(`--------------------------------------------------------`)
+    await deleteFiles(sprite.id)
+    const inputDir = `src/assets/images/sprites/${sprite.name}`
+    await generateSprite(inputDir, outputDir, sprite.id)
+  }
 }
 
-function deleteFiles(array, id) {
-  array.forEach((item) => {
-    fs.unlink(item, () => {})
-  })
-  console.log('--refreshed files: id:' + id)
+async function deleteFiles(id) {
+  const deleteArray = [
+    `src/assets/images/generated-sprites/spritesheet${id}.png`,
+    `src/assets/style/generated-sprites/spritesheet${id}.css`
+  ]
+
+  const deletePromises = deleteArray.map((item) => unlink(item))
+
+  await Promise.all(deletePromises)
+  console.log(`🔄 ID:${id}`)
 }
 
-function generateSprite(inputDir, outputDir, id) {
+async function generateSprite(inputDir, outputDir, id) {
   const imageFiles = glob.sync(`${inputDir}/**/*.png`)
 
   if (imageFiles.length === 0) {
-    console.error('--no images found')
-    return
+    console.error('-- No images found')
+    throw new Error('No images found')
   }
 
   const spritesmithOptions = {
@@ -95,34 +65,33 @@ function generateSprite(inputDir, outputDir, id) {
     }
   }
 
-  Spritesmith.run(spritesmithOptions, (err, result) => {
-    if (err) {
-      console.error('-- error: ', err)
-      return
-    }
+  const spritesmithRun = util.promisify(Spritesmith.run)
+  const result = await spritesmithRun(spritesmithOptions)
 
-    const spritePath = path.join(outputDir, `spritesheet${id}.png`)
-    fs.writeFileSync(spritePath, result.image)
-    console.log('--spritesheet ' + id + ' generated in: ', spritePath)
+  const spritePath = path.join(outputDir, `spritesheet${id}.png`)
+  await writeFile(spritePath, result.image)
+  console.log(`🖼️ Spritesheet ${id} generated in: ${spritePath}`)
 
-    const cssPath = `src/assets/style/spritesheet${id}.css`
-    const spriteData = result.coordinates
+  const cssPath = `src/assets/style/generated-sprites/spritesheet${id}.css`
+  const spriteData = result.coordinates
 
-    const cssContent = generateScssContent(spriteData, id)
-    fs.writeFileSync(cssPath, cssContent)
-    console.log('--css file ' + id + ' generated in:', cssPath)
-  })
+  const cssContent = generateCssContent(spriteData, id)
+  await writeFile(cssPath, cssContent)
+  console.log(`✨ CSS file ${id} generated in: ${cssPath}`)
+
+  const cssLocation = generateCssLocation(sprites)
+  await writeFile(outputCssFile, cssLocation)
 }
 
-function generateScssContent(spriteData, id) {
+function generateCssContent(spriteData, id) {
   let cssContent = ''
 
   for (const [filename, coordinates] of Object.entries(spriteData)) {
     const { x, y, width, height } = coordinates
-
     const className = path.basename(filename, path.extname(filename))
+
     cssContent += `.${defaultClass}.${className} {\n`
-    cssContent += `  background-image: url('../images/spritesheet${id}.png');\n`
+    cssContent += `  background-image: url('../../images/generated-sprites/spritesheet${id}.png');\n`
     cssContent += `  background-position: -${x}px -${y}px;\n`
     cssContent += `  width: ${width}px;\n`
     cssContent += `  height: ${height}px;\n`
@@ -131,3 +100,22 @@ function generateScssContent(spriteData, id) {
 
   return cssContent
 }
+
+function generateCssLocation(sprite) {
+  let cssContent = ''
+
+  for (const item of sprite) {
+    cssContent += `@import './generated-sprites/spritesheet${item.id}.css';\n`
+  }
+
+  return cssContent
+}
+
+generateSprites()
+  .catch((error) => {
+    console.error('-- Error:', error)
+  })
+  .finally(() => {
+    console.log(`--------------------------------------------------------`)
+    console.log('🎉 Finished!')
+  })
