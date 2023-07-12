09d0a5e1787c1bcad45d1981a768c36bf2dbe58c
LUCAS MONTEIRO RIBEIRO <lucas.monteiro58@gmail.com>
2023-07-12 19:41:05 -0300

    feat: ✨change to canvas

--- src/views/challenge/eco/EcoOneView.vue
+++ src/views/challenge/eco/EcoOneView.vue
@@ -1,94 +1,85 @@
 <script setup>
-import { moveLeftKeyboard, moveUpKeyboard, moveRightKeyboard, moveDownKeyboard } from '@/utils'
-import FloatingBall from './components/FloatingBall.vue'
+import robotImage from '@/assets/images/sprites/economia/planta_cabeca_robo.png'
 import { balls } from './consts'
 
-const moveRef = ref(null)
-const containerRef = ref(null)
-const showCursorKeyboard = ref(true)
-const stageBalls = ref(balls)
-const index = ref(0)
-const ballsRef = ref(null)
+const robot = new Image()
+robot.src = robotImage
 
-onMounted(() => {
-  window.addEventListener('keydown', handleKeyDown)
-})
+const showCursorKeyboard = ref(true)
+const canvasRef = ref(null)
+const canvasWidth = 1227
+const canvasHeight = 850
+const ctx = computed(() => canvasRef.value?.getContext('2d'))
+const x = ref(600)
+const y = ref(750)
+const allBalls = ref(balls)
+
+function draw() {
+  ctx.value?.clearRect(0, 0, canvasWidth, canvasHeight)
+  ctx.value?.drawImage(robot, x.value, y.value)
+  allBalls.value.forEach((el) => {
+    if (el.colided) return
+    drawBall(el)
+  })
+}
+
+function drawBall(ball) {
+  const ballImage = new Image()
+  ballImage.src = ball.img
+  ctx.value?.drawImage(ballImage, ball.x, ball.y)
+  ball.y += Math.sin(Date.now() / 1000) * 0.25
+  checkCollision(ball)
+}
 
 function handleKeyDown(event) {
   const { keyCode } = event
-  const element = moveRef.value
-  const containerElement = containerRef.value
+  showCursorKeyboard.value = false
+  if (keyCode === 37 && x.value > 0) {
+    x.value -= 10
+  } else if (keyCode === 38 && y.value > 0) {
+    y.value -= 10
+  } else if (keyCode === 39 && x.value < canvasWidth - 97) {
+    x.value += 10
+  } else if (keyCode === 40 && y.value < canvasHeight - 100) {
+    y.value += 10
+  }
+  draw()
+}
 
-  if (element && containerElement) {
-    showCursorKeyboard.value = false
-    const currentPosition = {
-      left: element.offsetLeft,
-      top: element.offsetTop
-    }
+function checkCollision(ball) {
+  const robotWidth = robot.width
+  const robotHeight = robot.height
+  const ballWidth = 101
+  const ballHeight = 101
 
-    switch (keyCode) {
-      case 37:
-        moveLeftKeyboard(currentPosition, containerElement)
-        break
-      case 38:
-        moveUpKeyboard(currentPosition, containerElement)
-        break
-      case 39:
-        moveRightKeyboard(currentPosition, element, containerElement)
-        break
-      case 40:
-        moveDownKeyboard(currentPosition, element, containerElement)
-        break
-      default:
-        return
-    }
-
-    updateElementPosition(element, currentPosition)
-    detectIntersect(ballsRef.value)
+  if (
+    x.value < ball.x + ballWidth &&
+    x.value + robotWidth > ball.x &&
+    y.value < ball.y + ballHeight &&
+    y.value + robotHeight > ball.y
+  ) {
+    allBalls.value = allBalls.value.map((el) => {
+      if (el.text === ball.text) return { ...el, colided: true }
+      else return el
+    })
   }
 }
 
-function updateElementPosition(element, currentPosition) {
-  element.style.left = `${currentPosition.left}px`
-  element.style.top = `${currentPosition.top}px`
-}
-
-function detectIntersect(el) {
-  const moveElement = moveRef.value
-  el.forEach((element) => {
-    const moveElementRect = moveElement.getBoundingClientRect()
-    const elRect = element?.getBoundingClientRect()
-
-    if (
-      moveElementRect.top < elRect.bottom &&
-      moveElementRect.bottom > elRect.top &&
-      moveElementRect.left < elRect.right &&
-      moveElementRect.right > elRect.left
-    ) {
-      console.log('colidiu')
-      index.value = 1
-    }
-  })
-}
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown)
+  setInterval(draw, 24)
+})
 </script>
 
 <template>
   <main class="spritesheet bg_fundo_economia1 flex justify-center items-center flex-col">
     <BaseImg img="planta_plantacasa_eco" class="ml-8">
-      <div ref="containerRef" class="w-[1227px] h-[850px] absolute top-[25px] left-[68px]">
-        <div ref="moveRef" class="absolute top-[750px] left-[600px]">
-          <BaseImg img="planta_cabeca_robo"></BaseImg>
-        </div>
-      </div>
-      <div
-        v-for="ball in stageBalls"
-        :class="ball.position"
-        ref="ballsRef"
-        :key="ball.text"
-        class="absolute"
-      >
-        <FloatingBall v-show="index === ball.stage" :img="ball.img" />
-      </div>
+      <canvas
+        ref="canvasRef"
+        :width="canvasWidth"
+        :height="canvasHeight"
+        class="absolute top-[25px] left-[68px]"
+      ></canvas>
     </BaseImg>
     <BaseImg img="planta_rectmoeda_menor" class="mr-[600px] relative scale-125">
       <BaseImg img="planta_moeda_eco" class="absolute top-2 left-2"></BaseImg>
--- src/views/challenge/eco/consts.js
+++ src/views/challenge/eco/consts.js
@@ -1,58 +1,80 @@
+import planta_item1_eco_bom from '@/assets/images/sprites/economia/planta_item1_eco_bom.png'
+import planta_item1_eco_ruim from '@/assets/images/sprites/economia/planta_item1_eco_ruim.png'
+import planta_item4_eco_bom from '@/assets/images/sprites/economia/planta_item4_eco_bom.png'
+import planta_item2_eco_bom from '@/assets/images/sprites/economia/planta_item2_eco_bom.png'
+import planta_item2_eco_ruim from '@/assets/images/sprites/economia/planta_item2_eco_ruim.png'
+import planta_item3_eco_ruim from '@/assets/images/sprites/economia/planta_item3_eco_ruim.png'
+import planta_item3_eco_bom from '@/assets/images/sprites/economia/planta_item3_eco_bom.png'
+
 export const balls = [
   {
     value: 10,
     type: 'good',
     stage: 0,
     text: '+10 por vender um brinquedo antigo',
-    img: 'planta_item1_eco_bom',
-    position: 'bottom-[100px] right-[320px]'
+    img: planta_item1_eco_bom,
+    position: 'bottom-[200px] right-[420px]',
+    x: 750,
+    y: 680
   },
   {
     value: -10,
     type: 'bad',
     stage: 1,
     text: '-10 por comprar dois brinquedos',
-    img: 'planta_item1_eco_ruim',
-    position: 'bottom-[60px] right-[100px]'
+    img: planta_item1_eco_ruim,
+    position: 'bottom-[160px] right-[200px]',
+    x: 1000,
+    y: 720
   },
   {
     value: 10,
     type: 'good',
     stage: 1,
     text: '+10 por passear com o cão da vizinha',
-    img: 'planta_item4_eco_bom',
-    position: 'bottom-[360px] right-[80px]'
+    img: planta_item4_eco_bom,
+    position: 'bottom-[460px] right-[180px]',
+    x: 1070,
+    y: 480
   },
   {
     value: 10,
     type: 'good',
     stage: 2,
     text: '+10 por ganhar moedas da avó',
-    img: 'planta_item2_eco_bom',
-    position: 'bottom-[560px] right-[430px]'
+    img: planta_item2_eco_bom,
+    position: 'bottom-[660px] right-[530px]',
+    x: 750,
+    y: 220
   },
   {
     value: -10,
     type: 'bad',
     stage: 3,
     text: '-10 por quebrar o vaso da mãe',
-    img: 'planta_item2_eco_ruim',
-    position: 'bottom-[710px] right-[310px]'
+    img: planta_item2_eco_ruim,
+    position: 'bottom-[810px] right-[410px]',
+    x: 830,
+    y: 70
   },
   {
     value: -10,
     type: 'bad',
     stage: 3,
     text: '-10 por gastar dinheiro com doces',
-    img: 'planta_item3_eco_ruim',
-    position: 'bottom-[550px] right-[790px]'
+    img: planta_item3_eco_ruim,
+    position: 'bottom-[650px] right-[890px]',
+    x: 360,
+    y: 230
   },
   {
     value: 10,
     type: 'good',
     stage: 3,
     text: '+10 por vender biscoitos na rua',
-    img: 'planta_item3_eco_bom',
-    position: 'bottom-[200px] right-[890px]'
+    img: planta_item3_eco_bom,
+    position: 'bottom-[300px] right-[990px]',
+    x: 260,
+    y: 580
   }
 ]
