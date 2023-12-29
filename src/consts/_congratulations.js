244f5353227cc0db180dccbd544365cba2298a48
Lucas Monteiro <lucas.ribeiro@drcuco.com.br>
2023-12-29 11:53:06 -0300

    feat: ✨Add congratulations messages and images

---
+++ src/consts/_congratulations.js
@@ -0,0 +1,85 @@
+export const congratulations = {
+  mei_0: {
+    text: 'Parabéns! Você deixou o terreno limpo e ajudou na <a>reciclagem</a> do lixo.',
+    tip: 'Reciclagem: transformar o lixo em um produto novo.',
+    stars: ['star-full', 'star-half', 'star-empty']
+  },
+  mei_1: {
+    text: 'Muito bem! Antes de cozinhar, vamos lavar o que você colheu.',
+    stars: ['star-full', 'star-full', 'star-empty']
+  },
+  mei_2: {
+    text: 'Parabéns! Agora as pessoas do seu bairro podem colher alimentos de graça nesse terreno.',
+    stars: ['star-full', 'star-full', 'star-full']
+  },
+  gov_0: {
+    text: 'Parabéns! Você conseguiu organizar a brincadeira.',
+    stars: ['star-half', 'star-empty', 'star-empty']
+  },
+  gov_1: {
+    text: 'Maravilha! Sua atitude ajudou muita gente que estava com fome!',
+    stars: ['star-full', 'star-empty', 'star-empty']
+  },
+  gov_2: {
+    text: 'A cidade adorou! Agora está todo mundo mais informado.',
+    stars: ['star-full', 'star-full', 'star-empty']
+  },
+  gov_3: {
+    text: 'Você é um grande líder! Fez ótimas escolhas para sua cidade. Continue assim.',
+    stars: ['star-full', 'star-full', 'star-full']
+  },
+  laz_0: {
+    text: 'Uau! Você conhece tudo mesmo!',
+    stars: ['star-full', 'star-empty', 'star-empty']
+  },
+  laz_1: {
+    text: 'Sua cidade é bem legal!',
+    stars: ['star-full', 'star-full', 'star-empty']
+  },
+  laz_2: {
+    text: 'Bom trabalho. Isso sim é ser um grande amigo do lazer.',
+    stars: ['star-full', 'star-full', 'star-full']
+  },
+  ino_0: {
+    text: 'Parabéns! Um inventor precisa ser um ótimo observador.',
+    textM: 'Parabéns! Uma inventora precisa ser uma ótima observadora',
+    stars: ['star-full', 'star-empty', 'star-empty']
+  },
+  ino_1: {
+    text: 'Maravilhoso! É preciso muita criatividade para fazer um foguete.',
+    stars: ['star-full', 'star-full', 'star-empty']
+  },
+  ino_2: {
+    text: 'É isso aí! Testar, testar e testar várias vezes ajuda a melhorar as invenções! Isso é persistência.',
+    stars: ['star-full', 'star-full', 'star-full']
+  },
+  sau_0: {
+    text: 'Muito bem. Você sabe como se cuidar!',
+    stars: ['star-full', 'star-half', 'star-empty']
+  },
+  sau_1: {
+    text: 'Maravilha! Comer bem e brincar mais, sem doenças nunca mais!',
+    stars: ['star-full', 'star-full', 'star-full']
+  },
+  edu_0: {
+    text: 'Por fora sua escola está incrível!',
+    stars: ['star-full', 'star-empty', 'star-empty']
+  },
+  edu_1: {
+    text: 'Agora sim está tudo certo para os alunos aprenderem melhor. Parabéns.',
+    stars: ['star-full', 'star-full', 'star-empty']
+  },
+  edu_2: {
+    text: 'Incrível! Eu queria estudar numa escola dessas!',
+    stars: ['star-full', 'star-full', 'star-full']
+  },
+  eco_0: {
+    text: 'Parabéns! Você juntou seu próprio dinheiro!',
+    stars: ['star-full', 'star-half', 'star-empty']
+  },
+  eco_1: {
+    text: 'Maravilha! Você <a>investiu</a> seu dinheiro em coisas muito legais.',
+    tip: 'Dar o seu dinheiro em troca de algo melhor pra você.',
+    stars: ['star-full', 'star-full', 'star-full']
+  }
+}
--- src/views/congratulation/CongratulationView.vue
+++ src/views/congratulation/CongratulationView.vue
@@ -1,29 +1,55 @@
 <script setup>
-import { getBadgeName } from '@/utils'
+import { congratulations } from '@/consts'
 
 const quizStore = useQuizStore()
 
-const badgeName = computed(() => {
-  return getBadgeName(quizStore.userBadge)
+const route = useRoute()
+
+const challenge = computed(() => {
+  return route.params.challenge
+})
+
+const level = computed(() => {
+  return route.params.level
 })
 
 const boxText = computed(() => {
-  return `Agora, me ajude a resolver alguns desafios d${badgeName.value[0]} ${badgeName.value[1]}.`
+  return congratulations[`${challenge.value}_${level.value - 1}`]?.text
 })
 
 const audio = computed(() => {
-  return `starts_desafios_${quizStore.userBadge}`
+  return `${challenge.value}_parabens${level.value}`
+})
+
+const imageBadge = computed(() => {
+  return challenge.value + '-badge'
+})
+
+const getStars = computed(() => {
+  return congratulations[`${challenge.value}_${level.value - 1}`]?.stars
 })
 </script>
 
 <template>
   <div class="flex justify-center items-center bg-congrats spritesheet">
     <BoxCongrats :line="boxText" :audio="audio" :time="5000" bg-congrats>
-      <BaseImg img="eco-badge" width="600px" class="absolute top-12 right-[300px]"></BaseImg>
+      <BaseImg :img="imageBadge" width="600px" class="absolute top-12 right-[300px]"></BaseImg>
       <div>
-        <BaseImg img="star-full" width="130px" class="absolute top-[610px] left-[950px]"></BaseImg>
-        <BaseImg img="star-full" width="130px" class="absolute top-[650px] left-[1100px]"></BaseImg>
-        <BaseImg img="star-full" width="130px" class="absolute top-[610px] left-[1250px]"></BaseImg>
+        <BaseImg
+          :img="getStars[0]"
+          width="130px"
+          class="absolute top-[610px] left-[950px]"
+        ></BaseImg>
+        <BaseImg
+          :img="getStars[1]"
+          width="130px"
+          class="absolute top-[650px] left-[1100px]"
+        ></BaseImg>
+        <BaseImg
+          :img="getStars[2]"
+          width="130px"
+          class="absolute top-[610px] left-[1250px]"
+        ></BaseImg>
       </div>
       <BaseButton
         name="btn-toggle-next"
