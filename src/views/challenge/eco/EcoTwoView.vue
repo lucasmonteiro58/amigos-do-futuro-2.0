5e67943e6e494fa16a6f756d7d3f6c466b799174
Lucas Monteiro <lucas.ribeiro@drcuco.com.br>
2023-12-31 11:32:20 -0300

    feat: ✨Add speech bubble component to EcoTwoView.vue

--- src/views/challenge/eco/EcoTwoView.vue
+++ src/views/challenge/eco/EcoTwoView.vue
@@ -1,5 +1,14 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <main
+    class="spritesheet monstrinhos_bg_monstrinhos_economia flex justify-center items-center flex-col"
+  >
+    <SpeechBubble
+      title="Coleção de bolinhas!"
+      description="Veja o que você ganha se colocar moedas nas máquinas de bolinhas."
+      audio="eco_help1"
+      :time="6100"
+    ></SpeechBubble>
+  </main>
 </template>
