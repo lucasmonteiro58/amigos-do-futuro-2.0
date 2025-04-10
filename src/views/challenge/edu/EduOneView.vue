d9534fe2ec519a2ad9d4a8049ea474f82194f7f9
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-04-10 16:58:53 -0300

    feat: ✨Enhance visibility functionality for buttons in EduOneView.vue and add TypeScript dependency

--- src/views/challenge/edu/EduOneView.vue
+++ src/views/challenge/edu/EduOneView.vue
@@ -10,23 +10,66 @@ const obj = ref({
 })
 
 function setVisibility(type) {
+  showCursorKeyboard.value = false
+  if (isMaxSelected.value && !obj.value[type]) {
+    return
+  }
   obj.value[type] = !obj.value[type]
 }
+
+function isVisible(type) {
+  return obj.value[type]
+}
+
+const isMaxSelected = computed(() => {
+  return (
+    obj.value.accessibility +
+      obj.value.paint +
+      obj.value.trees +
+      obj.value.bus +
+      obj.value.garden +
+      obj.value.park +
+      obj.value.trash >=
+    5
+  )
+})
+
+const showCursorKeyboard = ref(true)
 </script>
 
 <template>
-  <main class="spritesheet cenarioescola_fora flex justify-center items-center flex-col">
+  <main class="flex flex-col items-center justify-center spritesheet cenarioescola_fora">
     <div class="spritesheet barradeitens relative bottom-[-435px] flex justify-evenly">
       <BaseButton
-        name="escolaf_bt_acessib_normal"
+        :name="
+          isVisible('accessibility') ? 'escolaf_bt_acessib_check' : 'escolaf_bt_acessib_normal'
+        "
         @click="setVisibility('accessibility')"
       ></BaseButton>
-      <BaseButton name="escolaf_bt_pintar_normal" @click="setVisibility('paint')"></BaseButton>
-      <BaseButton name="escolaf_bt_arbori_normal" @click="setVisibility('trees')"></BaseButton>
-      <BaseButton name="escolaf_bt_bus_normal" @click="setVisibility('bus')"></BaseButton>
-      <BaseButton name="escolaf_bt_horta_normal" @click="setVisibility('garden')"></BaseButton>
-      <BaseButton name="escolaf_bt_parque_normal" @click="setVisibility('park')"></BaseButton>
-      <BaseButton name="escolaf_bt_lixo_png" @click="setVisibility('trash')"></BaseButton>
+      <BaseButton
+        :name="isVisible('paint') ? 'escolaf_bt_pintar_check' : 'escolaf_bt_pintar_normal'"
+        @click="setVisibility('paint')"
+      ></BaseButton>
+      <BaseButton
+        :name="isVisible('trees') ? 'escolaf_bt_arbori_check' : 'escolaf_bt_arbori_normal'"
+        @click="setVisibility('trees')"
+      ></BaseButton>
+      <BaseButton
+        :name="isVisible('bus') ? 'escolaf_bt_bus_check' : 'escolaf_bt_bus_normal'"
+        @click="setVisibility('bus')"
+      ></BaseButton>
+      <BaseButton
+        :name="isVisible('garden') ? 'escolaf_bt_horta_check' : 'escolaf_bt_horta_normal'"
+        @click="setVisibility('garden')"
+      ></BaseButton>
+      <BaseButton
+        :name="isVisible('park') ? 'escolaf_bt_parque_check' : 'escolaf_bt_parque_normal'"
+        @click="setVisibility('park')"
+      ></BaseButton>
+      <BaseButton
+        :name="isVisible('trash') ? 'escolaf_bt_lixo_check' : 'escolaf_bt_lixo_png'"
+        @click="setVisibility('trash')"
+      ></BaseButton>
     </div>
     <BaseImg img="escolaf_escolafora_antes" class="absolute bottom-[380px]"></BaseImg>
     <BaseImg
@@ -75,5 +118,10 @@ function setVisibility(type) {
       audio="edu_help0"
       :time="7500"
     ></SpeechBubble>
+    <CursorClick
+      v-if="showCursorKeyboard"
+      class="absolute scale-[1] opacity-70 bottom-[10px] right-[250px]"
+    ></CursorClick>
+    <BaseButton name="btn-toggle-next" class="absolute right-[20px]"></BaseButton>
   </main>
 </template>
