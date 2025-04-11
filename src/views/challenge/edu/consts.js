83a3e68a90ad606e39207e293fb278ea4f7fa244
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-04-11 13:06:00 -0300

    feat: ✨Implement feature selection functionality with dynamic rendering in EduOneView.vue and add consts.js for feature definitions

---
+++ src/views/challenge/edu/consts.js
@@ -0,0 +1,56 @@
+export const features = [
+  {
+    key: 'accessibility',
+    text: 'Facilidade para quem usa cadeira de rodas',
+    checkedIcon: 'escolaf_bt_acessib_check',
+    normalIcon: 'escolaf_bt_acessib_normal',
+    images: [
+      { src: 'escolaf_rampaacessibilidade', class: 'absolute bottom-[338px] left-[885px] z-[20]' }
+    ]
+  },
+  {
+    key: 'paint',
+    text: 'Cores bonitas',
+    checkedIcon: 'escolaf_bt_pintar_check',
+    normalIcon: 'escolaf_bt_pintar_normal',
+    images: [{ src: 'escolaf_escolafora_pintada', class: 'absolute bottom-[380px]' }]
+  },
+  {
+    key: 'trees',
+    text: 'Mais sombra e ar fresco',
+    checkedIcon: 'escolaf_bt_arbori_check',
+    normalIcon: 'escolaf_bt_arbori_normal',
+    images: [
+      { src: 'escolaf_arvores1', class: 'absolute bottom-[240px] left-[-60px]' },
+      { src: 'escolaf_arvores2', class: 'absolute bottom-[390px] right-[-20px]' }
+    ]
+  },
+  {
+    key: 'bus',
+    text: 'Mais aulas pela cidade',
+    checkedIcon: 'escolaf_bt_bus_check',
+    normalIcon: 'escolaf_bt_bus_normal',
+    images: [{ src: 'escolaf_onibusescolar', class: 'absolute bottom-[280px] left-[490px]' }]
+  },
+  {
+    key: 'garden',
+    text: 'Alimentos colhidos na horta',
+    checkedIcon: 'escolaf_bt_horta_check',
+    normalIcon: 'escolaf_bt_horta_normal',
+    images: [{ src: 'escolaf_horta', class: 'absolute bottom-[320px] left-[150px]' }]
+  },
+  {
+    key: 'park',
+    text: 'Um recreio divertido',
+    checkedIcon: 'escolaf_bt_parque_check',
+    normalIcon: 'escolaf_bt_parque_normal',
+    images: [{ src: 'escolaf_parquinho', class: 'absolute bottom-[320px] left-[1440px]' }]
+  },
+  {
+    key: 'trash',
+    text: 'Lixo separadinho',
+    checkedIcon: 'escolaf_bt_lixo_check',
+    normalIcon: 'escolaf_bt_lixo_png',
+    images: [{ src: 'escolaf_lixeirasseletivas', class: 'absolute bottom-[388px] left-[1155px]' }]
+  }
+]
