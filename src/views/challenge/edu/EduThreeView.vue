9ac536b78baf06f598fa3ff4957a5ebe3c57c46f
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-04-14 11:07:34 -0300

    Add new features and update styles for EduThreeView
    - Introduced new features for the EduThreeView component, including accessibility options, aesthetic improvements, and additional interactive elements.
    - Added new images and icons for the features, enhancing the visual representation.
    - Updated the CSS for the spritesheet to accommodate new icons and adjust existing positions.
    - Implemented logic to manage feature visibility and user interactions, including modals for selection limits.
    - Enhanced the consts.js file to include new feature definitions for the EduThreeView.

--- src/views/challenge/edu/EduThreeView.vue
+++ src/views/challenge/edu/EduThreeView.vue
@@ -1,5 +1,139 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue'
+import Popper from 'vue3-popper'
+import { featuresThree as features } from './consts'
+import ModalViewer from './components/ModalViewer.vue'
+
+const obj = ref(
+  features.reduce((acc, f) => {
+    acc[f.key] = false
+    return acc
+  }, {})
+)
+
+const hoverKey = ref(null)
+
+function setVisibility(type) {
+  showCursorKeyboard.value = false
+  if (isMaxSelected.value && !obj.value[type]) {
+    showLimitModal.value = true
+    return
+  }
+  obj.value[type] = !obj.value[type]
+}
+
+function isVisible(type) {
+  return obj.value[type]
+}
+
+const isMaxSelected = computed(() => {
+  return Object.values(obj.value).filter(Boolean).length >= 5
+})
+
+const showCursorKeyboard = ref(true)
+
+const visibleFeatures = computed(() => features.filter((feature) => obj.value[feature.key]))
+
+const showLimitModal = ref(false)
+const showMinModal = ref(false)
+
+function closeModal() {
+  showLimitModal.value = false
+  showMinModal.value = false
+}
+
+const router = useRouter()
+const showFinalModal = ref(false)
+
+function handleContinue() {
+  if (!isMaxSelected.value) {
+    showMinModal.value = true
+  } else {
+    showFinalModal.value = true
+  }
+}
+
+function handleSaveFinalChoice() {
+  router.push({ name: 'congratulation', params: { challenge: 'edu', level: 1 } })
+}
+</script>
 
 <template>
-  <div></div>
+  <main class="flex flex-col items-center justify-center spritesheet bg-cenario-sala">
+    <div class="spritesheet barradeitens relative bottom-[-435px] flex justify-evenly items-center">
+      <div
+        v-for="feature in features"
+        :key="feature.key"
+        class="relative flex items-center justify-center"
+      >
+        <Popper placement="top" offsetDistance="5" locked hover arrow>
+          <template #content>
+            <div class="px-4 py-4 text-center w-[300px]">
+              <div class="text-2xl">{{ feature.text }}</div>
+            </div>
+          </template>
+
+          <!-- Botão com ícone dinâmico -->
+          <BaseButton
+            class="inline-block"
+            :name="
+              hoverKey === feature.key
+                ? isVisible(feature.key)
+                  ? feature.minusIcon
+                  : feature.addIcon
+                : isVisible(feature.key)
+                  ? feature.checkedIcon
+                  : feature.normalIcon
+            "
+            @mouseenter="hoverKey = feature.key"
+            @mouseleave="hoverKey = null"
+            @click="setVisibility(feature.key)"
+          />
+        </Popper>
+      </div>
+    </div>
+
+    <template v-for="feature in visibleFeatures" :key="`imgs-${feature.key}`">
+      <BaseImg
+        v-for="(img, index) in feature.images"
+        :key="index"
+        :img="img.src"
+        :class="img.class"
+      />
+    </template>
+
+    <SpeechBubble
+      title="Minha sala de aula!"
+      description="Por último, adicione 5 coisas na sala de aula."
+      audio="edu_help2"
+      :time="6500"
+    />
+
+    <CursorClick
+      v-if="showCursorKeyboard"
+      class="absolute scale-[1] opacity-70 bottom-[10px] right-[250px]"
+    />
+
+    <BaseButton
+      name="btn-toggle-next"
+      class="absolute right-[40px] bottom-4"
+      width="180px"
+      @click="handleContinue"
+    />
+
+    <ModalAtention v-model="showLimitModal" @close="closeModal" content-font="font-exo2">
+      <div class="mt-8">Selecione somente 5 coisas para sua escola!</div>
+    </ModalAtention>
+
+    <ModalAtention v-model="showMinModal" @close="closeModal" content-font="font-exo2">
+      <div class="mt-8">Selecione 5 coisas para sua escola!</div>
+    </ModalAtention>
+
+    <ModalViewer
+      v-if="showFinalModal"
+      :features="visibleFeatures"
+      @close="showFinalModal = false"
+      @save="handleSaveFinalChoice"
+    />
+  </main>
 </template>
--- src/views/challenge/edu/consts.js
+++ src/views/challenge/edu/consts.js
@@ -149,3 +149,81 @@ export const featuresTwo = [
     images: [{ src: 'escolad_saladanca' }]
   }
 ]
+
+export const featuresThree = [
+  {
+    key: 'chair',
+    text: 'Facilidade para quem usa cadeira de rodas',
+    checkedIcon: 'escolaf_bt_acessib_check',
+    minusIcon: 'escolaf_bt_acessib_menos',
+    addIcon: 'escolaf_bt_acessib_add',
+    saveIcon: 'escolaf_acessib_save',
+    normalIcon: 'escolaf_bt_acessib_normal',
+    images: [
+      { src: 'escolaf_rampaacessibilidade', class: 'absolute bottom-[338px] left-[885px] z-[20]' }
+    ]
+  },
+  {
+    key: 'paint',
+    text: 'Cores bonitas',
+    checkedIcon: 'escolaf_bt_pintar_check',
+    minusIcon: 'escolaf_bt_pintar_menos',
+    addIcon: 'escolaf_bt_pintar_add',
+    saveIcon: 'escolaf_pintura_save',
+    normalIcon: 'escolaf_bt_pintar_normal',
+    images: [{ src: 'escolaf_escolafora_pintada', class: 'absolute bottom-[380px]' }]
+  },
+  {
+    key: 'trees',
+    text: 'Mais sombra e ar fresco',
+    checkedIcon: 'escolaf_bt_arbori_check',
+    minusIcon: 'escolaf_bt_arbori_menos',
+    addIcon: 'escolaf_bt_arbori_add',
+    saveIcon: 'escolaf_arvores_save',
+    normalIcon: 'escolaf_bt_arbori_normal',
+    images: [
+      { src: 'escolaf_arvores1', class: 'absolute bottom-[240px] left-[-60px]' },
+      { src: 'escolaf_arvores2', class: 'absolute bottom-[390px] right-[-20px]' }
+    ]
+  },
+  {
+    key: 'bus',
+    text: 'Mais aulas pela cidade',
+    checkedIcon: 'escolaf_bt_bus_check',
+    minusIcon: 'escolaf_bt_bus_menos',
+    addIcon: 'escolaf_bt_bus_add',
+    saveIcon: 'escolaf_onibuscolar_save',
+    normalIcon: 'escolaf_bt_bus_normal',
+    images: [{ src: 'escolaf_onibusescolar', class: 'absolute bottom-[280px] left-[490px]' }]
+  },
+  {
+    key: 'garden',
+    text: 'Alimentos colhidos na horta',
+    checkedIcon: 'escolaf_bt_horta_check',
+    minusIcon: 'escolaf_bt_horta_menos',
+    addIcon: 'escolaf_bt_horta_add',
+    saveIcon: 'escolaf_horta_save',
+    normalIcon: 'escolaf_bt_horta_normal',
+    images: [{ src: 'escolaf_horta', class: 'absolute bottom-[320px] left-[150px]' }]
+  },
+  {
+    key: 'park',
+    text: 'Um recreio divertido',
+    checkedIcon: 'escolaf_bt_parque_check',
+    minusIcon: 'escolaf_bt_parque_menos',
+    addIcon: 'escolaf_bt_parque_add',
+    saveIcon: 'escolaf_parquinho_save',
+    normalIcon: 'escolaf_bt_parque_normal',
+    images: [{ src: 'escolaf_parquinho', class: 'absolute bottom-[320px] left-[1440px]' }]
+  },
+  {
+    key: 'trash',
+    text: 'Lixo separadinho',
+    checkedIcon: 'escolaf_bt_lixo_check',
+    minusIcon: 'escolaf_bt_lixo_menos',
+    addIcon: 'escolaf_bt_lixo_add',
+    saveIcon: 'escolaf_lixeirasseletivas_save',
+    normalIcon: 'escolaf_bt_lixo_png',
+    images: [{ src: 'escolaf_lixeirasseletivas', class: 'absolute bottom-[388px] left-[1155px]' }]
+  }
+]
