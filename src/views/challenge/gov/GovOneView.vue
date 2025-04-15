e313d000c6bff044f59366cfe7facf671030eabd
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-04-15 17:29:20 -0300

    feat: ✨Add DragTest component and enhance GovOneView with drag-and-drop functionality

--- src/views/challenge/gov/GovOneView.vue
+++ src/views/challenge/gov/GovOneView.vue
@@ -1,5 +1,47 @@
-<script setup></script>
+<script setup>
+import { elementsOne as elements } from './consts'
+
+const droppeds = ref([])
+
+function verifyDropped(name) {
+  return droppeds.value.includes(name)
+}
+
+function onDrop(el) {
+  const { dataTransfer } = el
+  droppeds.value.push(dataTransfer)
+}
+</script>
 
 <template>
-  <div></div>
+  <main class="flex flex-col items-center justify-center spritesheet cenario1_governo">
+    <BaseImg img="campinho_campinho" class="absolute top-[464px]" />
+    <DragElement
+      v-for="el in elements"
+      :key="el.name"
+      :style="{ bottom: el.bottom, left: el.left }"
+      :dataTransfer="el.name"
+      :class="verifyDropped(el.name) ? 'hidden' : ''"
+    >
+      <div class="spritesheet" :class="el.image"></div>
+    </DragElement>
+    <DropElement
+      :expected="['children-red-1', 'children-red-2', 'children-red-3']"
+      @dropped="onDrop"
+      class="absolute w-[700px] h-[520px] top-[480px] left-[240px] bg-red-50"
+    >
+    </DropElement>
+    <DropElement
+      :expected="['children-yellow-1', 'children-yellow-2', 'children-yellow-3']"
+      @dropped="onDrop"
+      class="absolute w-[700px] h-[520px] top-[480px] left-[960px] bg-red-50"
+    >
+    </DropElement>
+    <SpeechBubble
+      title="Hora de brincar!"
+      description="Você pode ser líder no parquinho e organizar as brincadeiras. Comece separando os times."
+      audio="gov_help0"
+      :time="8300"
+    />
+  </main>
 </template>
---
+++ src/views/challenge/gov/consts.js
@@ -0,0 +1,44 @@
+export const elementsOne = [
+  {
+    name: 'children-red-1',
+    image: 'campinho_crianca1_vermelha',
+    type: 'red',
+    bottom: '630px',
+    left: '350px'
+  },
+  {
+    name: 'children-yellow-1',
+    image: 'campinho_crianca1_amarela',
+    type: 'yellow',
+    bottom: '630px',
+    left: '550px'
+  },
+  {
+    name: 'children-yellow-2',
+    image: 'campinho_crianca2_amarela',
+    type: 'yellow',
+    bottom: '630px',
+    left: '750px'
+  },
+  {
+    name: 'children-red-2',
+    image: 'campinho_crianca2_vermelha',
+    type: 'red',
+    bottom: '630px',
+    left: '950px'
+  },
+  {
+    name: 'children-yellow-3',
+    image: 'campinho_crianca3_amarela',
+    type: 'yellow',
+    bottom: '630px',
+    left: '1150px'
+  },
+  {
+    name: 'children-red-3',
+    image: 'campinho_crianca3_vermelha',
+    type: 'red',
+    bottom: '610px',
+    left: '1350px'
+  }
+]
