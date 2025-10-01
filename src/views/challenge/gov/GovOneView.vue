6332a0ee483b90af108499e64cb76b9a015cfa8b
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-10-01 19:55:49 -0300

    feat(gov): add flag wave animation and enhance drop functionality with new element properties

--- src/views/challenge/gov/GovOneView.vue
+++ src/views/challenge/gov/GovOneView.vue
@@ -1,15 +1,47 @@
 <script setup>
 import { elementsOne as elements } from './consts'
 
+const router = useRouter()
+
 const droppeds = ref([])
+const redFlagAnimating = ref(false)
+const yellowFlagAnimating = ref(false)
+
+const droppedsElements = computed(() =>
+  droppeds.value.map((name) => elements.find((el) => el.name === name))
+)
 
 function verifyDropped(name) {
   return droppeds.value.includes(name)
 }
 
+function animateFlag(flagRef) {
+  flagRef.value = false
+  nextTick(() => {
+    flagRef.value = true
+    setTimeout(() => {
+      flagRef.value = false
+    }, 1500)
+  })
+}
+
 function onDrop(el) {
   const { dataTransfer } = el
   droppeds.value.push(dataTransfer)
+
+  const droppedElement = elements.find((element) => element.name === dataTransfer)
+
+  if (droppedElement?.type === 'red') {
+    animateFlag(redFlagAnimating)
+  } else if (droppedElement?.type === 'yellow') {
+    animateFlag(yellowFlagAnimating)
+  }
+
+  if (droppeds.value.length === elements.length) {
+    setTimeout(() => {
+      router.push({ name: 'congratulation', params: { challenge: 'gov', level: 1 } })
+    }, 1000)
+  }
 }
 </script>
 
@@ -25,18 +57,39 @@ function onDrop(el) {
     >
       <div class="spritesheet" :class="el.image"></div>
     </DragElement>
+
+    <BaseImg
+      v-for="(dropped, index) in droppedsElements"
+      :key="index"
+      :img="dropped.image"
+      :style="{ bottom: dropped.droppedBottom, left: dropped.droppedLeft }"
+      class="absolute"
+    />
+
     <DropElement
       :expected="['children-red-1', 'children-red-2', 'children-red-3']"
       @dropped="onDrop"
-      class="absolute w-[700px] h-[520px] top-[480px] left-[240px] bg-red-50"
+      class="absolute w-[700px] h-[520px] top-[480px] left-[240px]"
     >
     </DropElement>
     <DropElement
       :expected="['children-yellow-1', 'children-yellow-2', 'children-yellow-3']"
       @dropped="onDrop"
-      class="absolute w-[700px] h-[520px] top-[480px] left-[960px] bg-red-50"
+      class="absolute w-[700px] h-[520px] top-[480px] left-[960px]"
     >
     </DropElement>
+
+    <BaseImg
+      img="campinho_bandeira_vermelha"
+      class="absolute left-[50px] bottom-[302px]"
+      :class="{ animation__flagWave: redFlagAnimating }"
+    />
+    <BaseImg
+      img="campinho_bandeira_amarela"
+      class="absolute left-[1680px] bottom-[302px]"
+      :class="{ animation__flagWave: yellowFlagAnimating }"
+    />
+
     <SpeechBubble
       title="Hora de brincar!"
       description="Você pode ser líder no parquinho e organizar as brincadeiras. Comece separando os times."
--- src/views/challenge/gov/consts.js
+++ src/views/challenge/gov/consts.js
@@ -4,41 +4,53 @@ export const elementsOne = [
     image: 'campinho_crianca1_vermelha',
     type: 'red',
     bottom: '630px',
-    left: '350px'
+    left: '350px',
+    droppedBottom: '140px',
+    droppedLeft: '480px'
   },
   {
     name: 'children-yellow-1',
     image: 'campinho_crianca1_amarela',
     type: 'yellow',
     bottom: '630px',
-    left: '550px'
+    left: '550px',
+    droppedBottom: '140px',
+    droppedLeft: '1300px'
   },
   {
     name: 'children-yellow-2',
     image: 'campinho_crianca2_amarela',
     type: 'yellow',
     bottom: '630px',
-    left: '750px'
+    left: '750px',
+    droppedBottom: '300px',
+    droppedLeft: '1050px'
   },
   {
     name: 'children-red-2',
     image: 'campinho_crianca2_vermelha',
     type: 'red',
     bottom: '630px',
-    left: '950px'
+    left: '950px',
+    droppedBottom: '350px',
+    droppedLeft: '800px'
   },
   {
     name: 'children-yellow-3',
     image: 'campinho_crianca3_amarela',
     type: 'yellow',
     bottom: '630px',
-    left: '1150px'
+    left: '1150px',
+    droppedBottom: '400px',
+    droppedLeft: '1450px'
   },
   {
     name: 'children-red-3',
     image: 'campinho_crianca3_vermelha',
     type: 'red',
     bottom: '610px',
-    left: '1350px'
+    left: '1350px',
+    droppedBottom: '420px',
+    droppedLeft: '450px'
   }
 ]
