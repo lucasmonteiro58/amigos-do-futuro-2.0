fb269f206bb34c13b721d236ec7c99226bbd2f00
Lucas Monteiro <lucas.monteiro58@gmail.com>
2025-11-20 15:29:46 -0300

    refactor: move MeiTwoView constants to consts.js and delete MeiThreeView.vue

--- src/views/challenge/mei/MeiTwoView.vue
+++ src/views/challenge/mei/MeiTwoView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import { useEffectsStore } from '@/stores/effects'
 import { useRouter } from 'vue-router'
 import { pipes as pipesAnimation } from '@/consts/_animations'
+import { pipesData, correctRotations } from '@/views/challenge/mei/consts'
 
 const effectsStore = useEffectsStore()
 const router = useRouter()
@@ -12,98 +13,7 @@ const showPipes = ref(true)
 const showAnimation = ref(false)
 const refPipesAnimation = ref(null)
 
-const pipes = ref([
-  {
-    id: 1,
-    sprite: 'sustent_pc1',
-    top: '59%',
-    left: '42%',
-    width: '11.545%',
-    rotation: 180,
-    type: 'T'
-  },
-  {
-    id: 2,
-    sprite: 'sustent_pc2',
-    top: '65.2%',
-    left: '26.65%',
-    width: '7.58%',
-    rotation: 90,
-    type: 'L'
-  },
-  {
-    id: 3,
-    sprite: 'sustent_pc3',
-    top: '65.2%',
-    left: '34.2%',
-    width: '7.95%',
-    rotation: 90,
-    type: 'I'
-  },
-  {
-    id: 4,
-    sprite: 'sustent_pc4',
-    top: '65.2%',
-    left: '53.55%',
-    width: '7.95%',
-    rotation: 90,
-    type: 'I'
-  },
-  {
-    id: 5,
-    sprite: 'sustent_pc5',
-    top: '65.2%',
-    left: '61.45%',
-    width: '7.58%',
-    rotation: 90,
-    type: 'L'
-  },
-  {
-    id: 6,
-    sprite: 'sustent_pc6',
-    top: '79.2%',
-    left: '26.65%',
-    width: '7.95%',
-    rotation: 90,
-    type: 'L'
-  },
-  {
-    id: 7,
-    sprite: 'sustent_pc7',
-    top: '84.6%',
-    left: '34.5%',
-    width: '7.9%',
-    rotation: 90,
-    type: 'I'
-  },
-  {
-    id: 8,
-    sprite: 'sustent_pc8',
-    top: '84.5%',
-    left: '42.3%',
-    width: '11.545%',
-    rotation: 180,
-    type: 'T'
-  },
-  {
-    id: 9,
-    sprite: 'sustent_pc9',
-    top: '84.5%',
-    left: '53.6%',
-    width: '7.9%',
-    rotation: 90,
-    type: 'I'
-  },
-  {
-    id: 10,
-    sprite: 'sustent_pc10',
-    top: '78.95%',
-    left: '61.1%',
-    width: '7.95%',
-    rotation: 90,
-    type: 'L'
-  }
-])
+const pipes = ref(pipesData)
 
 const handlePipeClick = (index) => {
   if (isCompleted.value) return
@@ -117,19 +27,6 @@ const handlePipeClick = (index) => {
 }
 
 const checkCompletion = () => {
-  const correctRotations = {
-    1: 0,
-    2: 0,
-    3: 180,
-    4: 180,
-    5: 0,
-    6: 0,
-    7: 180,
-    8: 0,
-    9: 180,
-    10: 0
-  }
-
   const allCorrect = pipes.value.every((pipe) => {
     return pipe.rotation === correctRotations[pipe.id]
   })
---
+++ src/views/challenge/mei/consts.js
@@ -0,0 +1,105 @@
+export const pipesData = [
+  {
+    id: 1,
+    sprite: 'sustent_pc1',
+    top: '59%',
+    left: '42%',
+    width: '11.545%',
+    rotation: 180,
+    type: 'T'
+  },
+  {
+    id: 2,
+    sprite: 'sustent_pc2',
+    top: '65.2%',
+    left: '26.65%',
+    width: '7.58%',
+    rotation: 90,
+    type: 'L'
+  },
+  {
+    id: 3,
+    sprite: 'sustent_pc3',
+    top: '65.2%',
+    left: '34.2%',
+    width: '7.95%',
+    rotation: 90,
+    type: 'I'
+  },
+  {
+    id: 4,
+    sprite: 'sustent_pc4',
+    top: '65.2%',
+    left: '53.55%',
+    width: '7.95%',
+    rotation: 90,
+    type: 'I'
+  },
+  {
+    id: 5,
+    sprite: 'sustent_pc5',
+    top: '65.2%',
+    left: '61.45%',
+    width: '7.58%',
+    rotation: 90,
+    type: 'L'
+  },
+  {
+    id: 6,
+    sprite: 'sustent_pc6',
+    top: '79.2%',
+    left: '26.65%',
+    width: '7.95%',
+    rotation: 90,
+    type: 'L'
+  },
+  {
+    id: 7,
+    sprite: 'sustent_pc7',
+    top: '84.6%',
+    left: '34.5%',
+    width: '7.9%',
+    rotation: 90,
+    type: 'I'
+  },
+  {
+    id: 8,
+    sprite: 'sustent_pc8',
+    top: '84.5%',
+    left: '42.3%',
+    width: '11.545%',
+    rotation: 180,
+    type: 'T'
+  },
+  {
+    id: 9,
+    sprite: 'sustent_pc9',
+    top: '84.5%',
+    left: '53.6%',
+    width: '7.9%',
+    rotation: 90,
+    type: 'I'
+  },
+  {
+    id: 10,
+    sprite: 'sustent_pc10',
+    top: '78.95%',
+    left: '61.1%',
+    width: '7.95%',
+    rotation: 90,
+    type: 'L'
+  }
+]
+
+export const correctRotations = {
+  1: 0,
+  2: 0,
+  3: 180,
+  4: 180,
+  5: 0,
+  6: 0,
+  7: 180,
+  8: 0,
+  9: 180,
+  10: 0
+}
